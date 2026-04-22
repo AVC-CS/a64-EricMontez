1c028cd5e02f63b693a3968d51b7217db9ffbba7
EricMontez <eric.alexander.montez@gmail.com>
2026-04-22 15:49:19 -0700

    A64 Done

--- main.cpp
+++ main.cpp
@@ -9,12 +9,12 @@ int main()
 	n3 = 30;
 	n4 = 40;
 
-	cout << "Original Values are " << n1 << "\t" << n2 << "\t" << n3 << "\t" << n4 << endl;
+	cout << "Original Values are :" << "\t" << n1 << "\t" << n2 << "\t" << n3 << "\t" << n4 << endl;
 
 	swapValues(n1, n2);
-	cout << "After changing two value n1 and n2 " << n1 << "\t" << n2 << endl;
+	cout << "After changing two value n1 and n2 :" << "\t" << n1 << "\t" << n2 << endl;
 	swapValues(n1, n2, n3);
-	cout << "After changing three value n1, n2 and n3 " << n1 << "\t" << n2 << "\t" << n3 << endl;
+	cout << "After changing three value n1, n2 and n3 :" << "\t" << n1 << "\t" << n2 << "\t" << n3 << endl;
 	swapValues(n1, n2, n3, n4);
-	cout << "After changing four value n1, n2, n3 and n4 " << n1 << "\t" << n2 << "\t" << n3 << "\t" << n4 << endl;
+	cout << "After changing four value n1, n2, n3 and n4 :" << "\t" << n1 << "\t" << n2 << "\t" << n3 << "\t" << n4 << endl;
 }
